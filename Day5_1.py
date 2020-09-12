aebf51fb0e4e5eb2bc79ebb32b855bc9c1e4ee97
phxxebe <70343049+phxxebe@users.noreply.github.com>
2020-09-13 03:17:30 +0530

    Add files via upload
    Batch 7 Day 5 Assignment 1

---
+++ Day5_1.py
@@ -0,0 +1,41 @@
+# day 5
+# assignment 1
+
+# it's a Match
+lst1 = [1,5,6,4,1,2,3,5]
+lst2 = [1,5,6,5,1,2,3,6]
+
+lst = [1,1,5]
+count = 0
+r=0
+for x in lst:
+    for y in lst1[r:]:
+        r+=1
+        if (x==y):
+            count+=1
+            break;
+        else:
+            pass
+        
+# it's Gone
+if(count==3):
+    print("it’s a Match")
+else:
+    print("it’s Gone")
+
+count = 0
+r=0
+for x in lst:
+    for y in lst2[r:]:
+        r+=1
+        if (x==y):
+            count+=1
+            break;
+        else:
+            pass
+        
+
+if(count==3):
+    print("it’s a Match")
+else:
+    print("it’s Gone")
